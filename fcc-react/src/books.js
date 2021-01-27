ac56493643dafc09a7441b29ef3413c7bd1cdbc4
lyndoncortez <lyndoncortez@yahoo.com>
2021-01-27 15:41:00 +0800

    Added Book Components

---
+++ fcc-react/src/books.js
@@ -0,0 +1,23 @@
+export const books = [
+  {
+    id: 1,
+    title: "I Love You to the Moon and Back",
+    author: "Amelia Hepworth",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+  },
+  {
+    id: 2,
+    title: "Change Sings: A Children's Anthem",
+    author: "Amanda Gorman",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/91wRcPETM0L._AC_UL200_SR200,200_.jpg",
+  },
+  {
+    id: 3,
+    title: "The Invisible Life of Addie LaRue",
+    author: "V.E. Schwab",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/91Ql48Y0mqL._AC_UL200_SR200,200_.jpg",
+  },
+];
--- fcc-react/src/index.js
+++ fcc-react/src/index.js
@@ -1,37 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { books } from "./books";
+import Book from "./Book";
 import "./index.css";
 
 function BookList() {
   return (
     <section className="bookList">
-      <Book
-        title="I Love You to the Moon and Back"
-        author="Amelia Hepworth"
-        img="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
-      />
-      <Book
-        title="Change Sings: A Children's Anthem"
-        author="Amanda Gorman"
-        img="https://images-na.ssl-images-amazon.com/images/I/91wRcPETM0L._AC_UL200_SR200,200_.jpg"
-      />
-      <Book
-        title="A Promise Land"
-        author="Barrack Obama"
-        img="https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg"
-      />
+      {books.map((book) => {
+        return <Book key={book.id} {...book} />;
+      })}
     </section>
   );
 }
-
-const Book = (props) => {
-  return (
-    <article className="book">
-      <img src={props.img} alt="" />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
-    </article>
-  );
-};
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
