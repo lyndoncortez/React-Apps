cf4285eb0c54c8dafd39b52814a6d565cf098aac
lyndoncortez <lyndoncortez@yahoo.com>
2021-01-29 15:47:09 +0800

    useState Tutorial

--- react-advanced-2020/src/tutorial/1-useState/setup/2-useState-basics.js
+++ react-advanced-2020/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [title, setTitle] = useState("Random Title");
+
+  const handleClick = () => {
+    setTitle("Hello World!");
+  };
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        Click Me
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
--- react-advanced-2020/src/tutorial/1-useState/setup/5-useState-counter.js
+++ react-advanced-2020/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+
+  const reset = () => {
+    setValue(0);
+  };
+
+  const decreaseValue = () => {
+    setValue((prevState) => {
+      return prevState - 1;
+    });
+  };
+
+  const increaseValue = () => {
+    setValue((prevState) => {
+      return prevState + 1;
+    });
+  };
+
+  return (
+    <>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>Regular Counter</h2>
+        <h1>{value}</h1>
+        <button id="minus" className="btn" onClick={decreaseValue}>
+          -
+        </button>
+        <button className="btn" onClick={reset}>
+          C
+        </button>
+        <button id="add" className="btn" onClick={increaseValue}>
+          +
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
