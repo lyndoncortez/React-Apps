b9d1690eebd7cb8d61f2ede16358a7b10920ede9
lyndoncortez <lyndoncortez@yahoo.com>
2021-01-30 14:10:45 +0800

    added React projects repository

--- react-advanced-2020/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-advanced-2020/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,20 @@
-import React, { useState, useEffect } from 'react';
-// by default runs after every re-render
-// cleanup function
-// second parameter
+import React, { useState, useEffect } from "react";
+
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    if (value > 0) {
+      document.title = `New Message(${value})`;
+    }
+  });
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        Click me
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
