bcb5f2b9d42bf026e5d279aed270dae91eb8b907
lyndoncortez <lyndoncortez@yahoo.com>
2021-02-01 21:33:24 +0800

    Adding Logic

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import QuestionCard from "./components/QuestionCard";
+import { fetchQuizQuestions } from "./API";
+import { Questionstate, Difficulty } from "./API";
 
-function App() {
-  return <div className="App">Quiz</div>;
-}
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<Questionstate[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+
+  const startQuiz = async () => {};
+
+  const checkAnswer = (event: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
+  return (
+    <div className="App">
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startQuiz}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions...</p>
+      {/* <QuestionCard
+        questionNum={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      /> */}
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
+};
 
 export default App;
