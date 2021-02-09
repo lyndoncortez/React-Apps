8797e08028f5b2452abb5798fa5967860dee67e6
lyndoncortez <lyndoncortez@yahoo.com>
2021-02-10 00:16:58 +0800

    Update

--- react-projects-master/9-color-generator/setup/src/App.js
+++ react-projects-master/9-color-generator/setup/src/App.js
@@ -1,10 +1,22 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-  return <h2>color generator project</h2>
+  const [state, setstate] = useState(initialState);
+  const [state, setstate] = useState(initialState);
+  const [state, setstate] = useState(initialState);
+
+  return (
+    <section className="container">
+      <h3>Color Generator</h3>
+      <form>
+        <input type="text" className="null" placeholder="#f15025" value />
+        <button className="btn">Submit</button>
+      </form>
+    </section>
+  );
 }
 
-export default App
+export default App;
